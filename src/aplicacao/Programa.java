6c3f9171ba6020bca8f039c26fd1e42e7e2d768a
Raif Zeidam <mrzeidam2@gmail.com>
2022-04-28 14:03:12 -0300

    findById

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,21 +1,16 @@
 package aplicacao;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
-import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Departamento obj = new Departamento(1, "Livros");
-		
-		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+		
+		Vendedor vendedor = vendedorDao.findById(3);
 		
 		System.out.println(vendedor);
 
