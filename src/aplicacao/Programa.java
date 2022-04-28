b07363a5d641c494227829ac7ff1d21a2ee8f42f
Raif Zeidam <mrzeidam2@gmail.com>
2022-04-28 14:31:41 -0300

    findByDepartamento

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,7 +1,10 @@
 package aplicacao;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
@@ -11,9 +14,13 @@ public class Programa {
 		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
 		
 		Vendedor vendedor = vendedorDao.findById(3);
-		
 		System.out.println(vendedor);
 
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> lista = vendedorDao.findByDepartamento(departamento);
+		for (Vendedor obj : lista) {
+			System.out.println(obj);
+		}
 	}
 
 }
