93c3aaebb49296597d52d7c65f1f0245d058291e
Raif Zeidam <mrzeidam2@gmail.com>
2022-04-28 13:36:07 -0300

    interfaces

---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Vendedor;
+
+public interface VendedorDao {
+
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void deleteById(Integer id);
+	Vendedor findById(Integer Id);
+	List<Vendedor> findAll();
+}
